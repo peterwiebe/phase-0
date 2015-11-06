a48fe03b7be4e5cc8fa45daf9f8f8421552f0b15
peterwiebe <pjwiebe@gmail.com>
2015-11-06 11:13:30 -0800

    Update DOM manipulation page

--- week-9/home_page.js
+++ week-9/home_page.js
@@ -37,15 +37,3 @@ for (var i = 0; i < elements.length; i++) {
 
 var tmpl = document.getElementById('hidden');
 document.body.appendChild(tmpl.content.cloneNode(true));
-
-// REFLECTION
-/*
-- What did you learn about the DOM?
-    I learned to think of the DOM to be like a tree of parent and children nodes. As well as how to traverse those nodes and find the specific nodes that I am looking to update.
-
-- What are some useful methods to use to manipulate the DOM?
-    Some useful methods we used included:
-      - getElementById: get an HTML element with a specific ID
-      - getElementsByClassName: get HTML elements with a specific CLASS
-      - getElementsByTagName: get specific HTML elements with a specific tag
-*/
